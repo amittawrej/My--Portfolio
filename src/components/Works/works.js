c482a8862948195d779c2bffd75aa5ed79f1efc6
amittawrej <amittawrej1999@gmail.com>
2024-01-30 20:37:41 +0530

    Third commit

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -7,10 +7,18 @@ const Works = () => {
     <section id='works'>
         <h2 className="worksTitle">My Portfolio</h2>
         <span className="workDesc">I take pride in paying attention to the smallest details</span>
-        <div className="worksImgs">
-            <img src={GoldGym} alt="GoldGym" className="worksImg" />
-            <img src={Todo} alt="Todo" className="worksImg" />
+    <div className="worksImgs">
+    <   div className="workItem">
+            <img src={GoldGym} alt="GoldGym"            className="worksImg" />
+            <p className='imgDesc'>Gold Gym</p>
         </div>
+        <div className="workItem">
+            <img src={Todo} alt="Todo" className="worksImg" />
+            <p className='imgDesc'>Task Manager App</p>
+        </div>
+    </div>
+    <button className="worksBtn">See More</button>
+
     </section>
   )
 }
